07171270e389d51aa5892538b46be9f0bf137bbe
Victor Hernandez <v.hernandez@cgiar.org>
2023-03-13 23:29:59 -0500

    GAP-35 api.py

---
+++ src/api.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, redirect
+from flask_restful import Api
+from flask_cors import CORS
+from mongoengine import connect
+from config import config
+
+from modules.country import Countries
+from modules.crops import Crops, Groups, GroupsByCropsIds, GroupsByIDCrop
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+api.add_resource(Countries, '/countries')
+api.add_resource(Crops, '/crops')
+api.add_resource(Groups, '/groups')
+api.add_resource(GroupsByCropsIds, '/groupsbyid', '/groupsbyid/<crop_id>')
+api.add_resource(GroupsByIDCrop, '/groupsbyid2' , '/groupsbyid2/<string:id>')
+
+
+if __name__ == '__main__':
+    connect(host=config['CONNECTION_DB'])
+    print("Connected DB")
+    
+    if config['DEBUG']:
+        app.run(threaded=True, port=config['PORT'], debug=config['DEBUG'])
+    else:
+        app.run(host=config['HOST'], port=config['PORT'],
+                debug=config['DEBUG'])
