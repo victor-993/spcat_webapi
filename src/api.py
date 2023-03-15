e4ec62e5228027793c8e0ad9fc4c6302e0a975f4
Victor Hernandez <v.hernandez@cgiar.org>
2023-03-15 12:26:06 -0500

    GAP-35 init swagger

--- src/api.py
+++ src/api.py
@@ -1,9 +1,10 @@
 
-from flask import Flask, redirect
+from flask import Flask, redirect, send_from_directory
 from flask_restful import Api
 from flask_cors import CORS
 from mongoengine import connect
 from config import config
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from modules.country import Countries
 from modules.crops import Crops, Groups, GroupsByIDCrop
@@ -12,6 +13,23 @@ from modules.accessions import AccessionsByIDCrop, AccessionsByIDGroup
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={'app_name': "Gap Analysis Web API"}
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+@app.route('/')
+def home():
+    return redirect("/swagger")
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static',path)
 
 api.add_resource(Countries, '/countries')
 api.add_resource(Crops, '/crops')
